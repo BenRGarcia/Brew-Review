6ad3360b9cc82b9f02ceee78e545d6215bdd9dcf
Ben Garcia <ben@seebenprogram.com>
2018-05-01 11:38:28 -0400

    require favicon pkg

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 var sassMiddleware = require('node-sass-middleware')
 var hbs = require('hbs')
+var favicon = require('serve-favicon')
 require('dotenv').config()
 
 var indexRouter = require('./routes/index')
 var usersRouter = require('./routes/users')
 
 var app = express()
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
